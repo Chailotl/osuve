ea21f22a4a6144d5839af96b24e78bd01f6fb5c5
Chailotl <7232280+Chailotl@users.noreply.github.com>
2018-03-27 13:36:31 -0400

    Revert "Revert "Better chunk data security""
    This reverts commit 66dc6ccab2c456f4687a29556957e8aa7bbc4d6f.

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -15,10 +15,16 @@ public class Chunk : MonoBehaviour
 	private Mesh _mesh;
 	private MeshCollider _col;
 	private bool _updateMesh;
+	private bool _loaded;
+
+	private int _chunkX;
+	private int _chunkY;
+	private int _chunkZ;
+	private World.DataChunk _chunkData;
+
 	public bool isolateMesh;
 	private bool _updateIso;
 
-<<<<<<< HEAD
 	public void LoadData(World.Int3 pos, World.DataChunk chunkData)
 	{
 		// One-time only!
@@ -29,13 +35,10 @@ public class Chunk : MonoBehaviour
 			_chunkX = pos.x;
 			_chunkY = pos.y;
 			_chunkZ = pos.z;
-=======
-	public int chunkX;
-	public int chunkY;
-	public int chunkZ;
->>>>>>> parent of 83ba107... Better chunk data security
 
-	public World.DataChunk chunkData;
+			_chunkData = chunkData;
+		}
+	}
 
 	void Start()
 	{
@@ -62,9 +65,9 @@ public class Chunk : MonoBehaviour
 	public void GenerateMesh()
 	{
 		// Check if data chunk blocks are generated
-		if (!chunkData.IsGenerated())
+		if (!_chunkData.IsGenerated())
 		{
-			chunkData.GenerateBlocks();
+			_chunkData.GenerateBlocks();
 		}
 
 		int chunkSize = World.GetChunkSize();
@@ -124,7 +127,7 @@ public class Chunk : MonoBehaviour
 		if (x >= 0 && x <= 15 && y >= 0 && y <= 15 && z >= 0 && z <= 15)
 		{
 			// In bounds, we have the data available to us
-			return chunkData.blocks[x, y, z];
+			return _chunkData.blocks[x, y, z];
 		}
 		else if (isolateMesh)
 		{
@@ -133,7 +136,7 @@ public class Chunk : MonoBehaviour
 		else
 		{
 			// Outside of bounds, need to fetch
-			return World.GenerateBlock(x + chunkX, y + chunkY, z + chunkZ);
+			return World.GenerateBlock(x + _chunkX, y + _chunkY, z + _chunkZ);
 			//return World.Block(chunkX, chunkY, chunkZ, x, y, z);
 		}
 	}
