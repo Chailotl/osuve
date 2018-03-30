462c96ea87c11c8aaef28b289378301cdc2ef6a5
Raus <bloxxer14@gmail.com>
2018-03-30 00:43:30 -0400

    Made these as separate classes

---
+++ Assets/Scripts/Int2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Int2 : IEquatable<Int2>
+{
+	public int x, z;
+
+	public Int2(int x1, int z1)
+	{
+		x = x1; z = z1;
+	}
+
+	public Int2(Vector3 vec)
+	{
+		x = Mathf.FloorToInt(vec.x);
+		z = Mathf.FloorToInt(vec.z);
+	}
+
+	public override string ToString()
+	{
+		return "(" + x + ", " + z + ")";
+	}
+
+	public bool Equals(Int2 other)
+	{
+		return (this.x == other.x && this.z == other.z);
+	}
+}
