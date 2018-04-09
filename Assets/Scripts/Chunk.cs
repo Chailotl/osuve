823e3f14bcf7d487ebb39421b12d72861d3936e2
Chailotl <7232280+Chailotl@users.noreply.github.com>
2018-04-09 19:10:28 -0400

    Removed unnecessary using

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
